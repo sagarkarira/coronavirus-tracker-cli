d58e38185e356909e5fc47cf3309d2922279742f
Alexandra Parker <alexandra.parker@navy.mil>
2020-03-17 15:54:33 -0700

    add basic CLI support, --emojis flag, [country] positional argument

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,56 @@
 #!/usr/bin/env node
 
 const { getCompleteTable } = require('../lib/corona');
+const { getCountryTable } = require('../lib/byCountry');
+const { lookupCountry } = require('../lib/helpers');
 
-getCompleteTable()
+const {argv} = require('yargs')
+  .command('$0 [country]', 'show COVID-19 statistics for the world or the given country', yargs =>
+    yargs.positional('country', {
+      coerce(arg) {
+      console.log(arg);
+        if('ALL' === arg.toUpperCase()) {
+          return 'ALL';
+        }
+
+        const country = lookupCountry(arg);
+
+        if(!country) {
+          throw new Error(`Country '${arg}' not found.
+            Try full country name or country code.
+            Ex:
+            - UK: for United Kingdom
+            - US: for United States of America.
+          - India: for India.`);
+        }
+
+        return country.iso2;
+      },
+      describe: 'which country to show statistics for',
+      default: 'all',
+      type: 'string'
+    })
+  )
+  .options({
+    e: {
+      alias: 'emojis',
+      describe: 'enable the use of emojis in the table (may break alignment in some terminals)',
+      default: false,
+      type: 'boolean'
+    },
+    c: {
+      alias: 'color',
+      describe: 'enable the use of color in the table.',
+      type: 'boolean'
+    }
+  })
+  .strict()
+  .help('help');
+
+console.log(argv);
+
+const {emojis, country} = argv;
+
+(country === 'all' ? getCompleteTable(argv) : getCountryTable(country, argv))
   .then(console.log)
   .catch(console.error);
