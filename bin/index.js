c0af963668f009665d5fcd22c02db87bdd099299
sagark <sagarkarira1992@gmail.com>
2020-03-19 00:12:55 +0530

    fix changes to country filter on cli, changes to readme

--- bin/index.js
+++ bin/index.js
@@ -1,32 +1,32 @@
 #!/usr/bin/env node
-
+const yargonaut = require('yargonaut')
+  .style('green');
+const yargs = require('yargs');
+const chalk = require('chalk');
 const { getCompleteTable } = require('../lib/corona');
 const { getCountryTable } = require('../lib/byCountry');
 const { lookupCountry } = require('../lib/helpers');
 
-const {argv} = require('yargs')
-  .command('$0 [country]', 'show COVID-19 statistics for the world or the given country', yargs =>
+const { argv } = yargs
+  .command('$0 [country]','Tool to COVID-19 statistics for the world or the given country', yargs =>
     yargs.positional('country', {
       coerce(arg) {
-      console.log(arg);
-        if('ALL' === arg.toUpperCase()) {
+        if ('ALL' === arg.toUpperCase()) {
           return 'ALL';
         }
-
         const country = lookupCountry(arg);
-
-        if(!country) {
-          throw new Error(`Country '${arg}' not found.
-            Try full country name or country code.
-            Ex:
-            - UK: for United Kingdom
-            - US: for United States of America.
-          - India: for India.`);
+        if (!country) {
+          let error = `Country '${arg}' not found.\n`;
+          error += 'Try full country name or country code.\n';
+          error += 'Ex:\n';
+          error += '- UK: for United Kingdom \n';
+          error += '- US: for United States of America.\n';
+          error += '- Italy: for Italy.\n';
+          throw new Error(chalk.red.bold(error));
         }
-
         return country.iso2;
       },
-      describe: 'which country to show statistics for',
+      describe: 'Filter table by country',
       default: 'all',
       type: 'string'
     })
@@ -34,19 +34,24 @@ const {argv} = require('yargs')
   .options({
     e: {
       alias: 'emojis',
-      describe: 'enable the use of emojis in the table (may break alignment in some terminals)',
+      describe: 'Show emojis in table',
       default: false,
       type: 'boolean'
     },
     c: {
       alias: 'color',
-      describe: 'enable the use of color in the table.',
+      describe: 'Show colors formatted output',
       type: 'boolean'
     }
   })
   .strict()
   .help('help');
 
-(country === 'all' ? getCompleteTable(argv) : getCountryTable(country, argv))
+const { emojis, country } = argv;
+(
+  country === 'ALL'
+    ? getCompleteTable(emojis)
+    : getCountryTable(country, emojis)
+)
   .then(console.log)
   .catch(console.error);
