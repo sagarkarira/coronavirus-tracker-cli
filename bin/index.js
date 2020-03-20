79b91bbaca323f676292a0983e653788fe73a608
sagark <sagarkarira1992@gmail.com>
2020-03-20 19:36:24 +0530

    added back yargonaut

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+// eslint-disable-next-line no-unused-vars
+const yargonaut = require('yargonaut').style('green');
 const yargs = require('yargs');
 const chalk = require('chalk');
 const { getCompleteTable } = require('../lib/corona');
